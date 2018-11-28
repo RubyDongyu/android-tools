6e7b1b2be30fd9a30b59eb87ed0f5f4a6c6731bf
ruby.dongyu <dongyu@bbktel.com>
2018-11-28 18:20:27 +0800

    config bin path.

--- src/adb.js
+++ src/adb.js
@@ -2,9 +2,38 @@ var console = require('console');
 var net = require('net');
 var events = require('events');
 var util = require('util');
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+var config = require('./config');
+
+var adb = `${config.BINPATH}adb`;
 
 exports.devices = devices;
 
 function devices() {
     console.log('adb devices');
+    //command = spawn('adb', ['devices']);
+    command = `${adb} devices`;
+    console.log(command);
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+	    console.log(`adb devices error: ${stderr}`)
+	} else {
+	    console.log(`adb devices: ${stdout}`);
+	}
+    });
+    /**
+
+    command.stdout.on('data', (data) => {
+        console.log(`adb devices return: ${data}`);
+    });
+
+    command.stderr.on('data', (data) => {
+        console.log(`adb devices return: ${data}`);
+    });
+
+    command.on('exit', (code, signal) => {
+        console.log(`adb devices return: ${code}`);
+    });
+    */
 }
--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ var os = require('os');
 exports.OSTYPE = os.type();
 exports.OSPLATFORM = os.platform();
 exports.OSARCH = os.arch();
+exports.BINPATH = `${__dirname}/../bin/${os.platform()}/`;
